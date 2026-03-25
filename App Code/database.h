e599fc42e3dd15fdaef721efd98a41db3acda43c
ParkerE8076 <pellinger2@lssu.edu>
2026-03-24 21:52:44 -0400

    Update database.h
    new code

--- App Code/database.h
+++ App Code/database.h
@@ -6,36 +6,36 @@
 #include <optional>
 namespace Database
 {
-    class Reservation
-    {
-      public:
-        int id;
-        QString name;
-        QString room;
-        QDate startDate;
-        QTime startTime;
-        QTime endTime;
-        QDate endDate;
-        int frequency;
-        int byWeekday;
-        bool overwriteable;
+class Reservation
+{
+public:
+    int id;
+    QString name;
+    QString room;
+    QDate startDate;
+    QTime startTime;
+    QTime endTime;
+    QDate endDate;
+    int frequency;
+    int byWeekday;
+    bool overwriteable;
 
-        static bool create();
-        static std::optional<Reservation> insert(const Reservation &r);
-        static QList<Database::Reservation> retrieve();
-        static bool update(const Reservation &r);
-        static void remove();
-    };
+    static bool create();
+    static std::optional<Reservation> insert(const Reservation &r);
+    static QList<Database::Reservation> retrieve();
+    static bool update(const Reservation &r);
+    static void remove();
+};
 
-    class Room
-    {
-      public:
-        static bool create();
-        static bool insert(const QString &name, QString &error);
-        static QStringList retrieve();
-        static void update();
-        static bool remove(const QString &name);
-    };
+class Room
+{
+public:
+    static bool create();
+    static bool insert(const QString &name, QString &error);
+    static QStringList retrieve();
+    static void update();
+    static bool remove(const QString &name);
+};
 }
 
 #endif // DATABASE_H
